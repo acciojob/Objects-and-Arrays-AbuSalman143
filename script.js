7c2893c4c2732502d5b5410b906114316948c009
Abu Salman <121804012+AbuSalman143@users.noreply.github.com>
2023-07-09 14:57:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,6 +7,15 @@ const person = {
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
+// Create a reference variable team to the players array
+let team = players;
+
+// Create a copy variable team1 of players array
+let team1 = players.slice();
+
+// Create a copy variable cap1 of person object
+let cap1 = Object.assign({}, person);
+
 window.players = players;
 window.person = person;
 window.team = team;
